0bed67b9f5cd6b0c9913f3414159107abf80f41e
toro_ponz <toro-ponz@users.noreply.github.com>
2022-10-15 21:03:34 +0900

    Modify window size on windows.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
 
 import 'package:calculator/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows) {
+    setWindowMinSize(const Size(540, 960));
+    setWindowMaxSize(const Size(540, 960));
+  }
+
   runApp(const CalculatorApp());
 }
